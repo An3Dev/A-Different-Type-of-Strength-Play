713a2092edcb41b2643fb66d714043fa73063ff9
An3Dev <An3Developer@gmail.com>
2018-01-16 03:00:33 -0800

    Something

---
+++ Assets/Scripts/GameControllerSceneOne.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControllerSceneOne : MonoBehaviour {
+
+	GameObject jacob, sophia, anniah;
+	Transform camera1;
+
+	Animation jacobWalk, jacobIdle, sophiaIdle, anniahIdle;
+
+	// Use this for initialization
+	void Start () {
+		jacob = GameObject.Find ("Jacob");
+		sophia = GameObject.Find ("Sophia");
+		anniah = GameObject.Find ("Anniah");	
+
+		camera1 = Camera.main.transform;
+
+
+
+		//jacob.transform.position -= jacob.transform.forward;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
